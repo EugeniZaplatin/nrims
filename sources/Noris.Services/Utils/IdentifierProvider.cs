b1456344fd282173f9b4023086f2c5fe37594b48
zef <zef@kuzdrav.ru>
2015-03-10 00:42:13 +0700

    #3 - realize IIdentifierProvider

---
+++ sources/Noris.Services/Utils/IdentifierProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Noris.Dao.Api;
+using Noris.Unity.Attributes;
+
+namespace Noris.Dao
+{
+    [Service(typeof(IIdentifierProvider))]
+    public class IdentifierProvider : IIdentifierProvider
+    {
+        private readonly object _lock = new object();
+        private readonly SequentialGuidGenerator _generator = new SequentialGuidGenerator();
+        public Guid NewIdentifier()
+        {
+            lock (_lock)
+            {
+                Thread.Sleep(1);
+                return _generator.NewGuid(SequentialGuidType.SequentialAsString);
+            }
+        }
+
+        public IEnumerable<Guid> NewIdentifiers(int count)
+        {
+            lock (_lock)
+            {
+                Thread.Sleep(1);
+                var guids = new HashSet<Guid>();
+                while (guids.Count < count)
+                {
+                    guids.Add(_generator.NewGuid(SequentialGuidType.SequentialAsString));
+                }
+                return guids.OrderBy(o => o);
+            }
+        }
+    }
+}
---
+++ sources/Noris.Services/Utils/SequentialGuidGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noris.Dao
+{
+    public enum SequentialGuidType
+    {
+        SequentialAsString,
+        SequentialAsBinary,
+        SequentialAtEnd
+    }
+
+    public class SequentialGuidGenerator
+    {
+        private readonly RNGCryptoServiceProvider _rng;
+        private readonly long _initialTick = new DateTime(2000, 01, 01).Ticks;
+
+        public SequentialGuidGenerator()
+        {
+            _rng = new RNGCryptoServiceProvider();
+        }
+
+        public Guid NewGuid(SequentialGuidType guidType)
+        {
+            var randomBytes = new byte[10];
+            _rng.GetBytes(randomBytes);
+
+            var timestamp = (DateTime.UtcNow.Ticks - _initialTick) / 10000L;
+            var timestampBytes = BitConverter.GetBytes(timestamp);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(timestampBytes);
+            }
+
+            var guidBytes = new byte[16];
+
+            switch (guidType)
+            {
+                case SequentialGuidType.SequentialAsString:
+                case SequentialGuidType.SequentialAsBinary:
+                    Buffer.BlockCopy(timestampBytes, 2, guidBytes, 0, 6);
+                    Buffer.BlockCopy(randomBytes, 0, guidBytes, 6, 10);
+
+                    if (guidType == SequentialGuidType.SequentialAsString
+                        && BitConverter.IsLittleEndian)
+                    {
+                        Array.Reverse(guidBytes, 0, 4);
+                        Array.Reverse(guidBytes, 4, 2);
+                    }
+                    break;
+
+                case SequentialGuidType.SequentialAtEnd:
+                    Buffer.BlockCopy(randomBytes, 0, guidBytes, 0, 10);
+                    Buffer.BlockCopy(timestampBytes, 2, guidBytes, 10, 6);
+                    break;
+            }
+
+            return new Guid(guidBytes);
+        }
+    }
+}
