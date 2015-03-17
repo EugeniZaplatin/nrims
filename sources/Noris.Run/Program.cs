32d34c01e9a014a834df8246aaee3a463ff0a6c4
zef <zef@kuzdrav.ru>
2015-03-17 07:34:51 +0700

    #4 Add builder class for forming dynamic part of directory record structure

--- sources/Noris.Run/Program.cs
+++ sources/Noris.Run/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Noris.Data.Entity;
 
 namespace Urish.Diagnostic.Run
 {
@@ -9,6 +10,20 @@ namespace Urish.Diagnostic.Run
     {
         static void Main(string[] args)
         {
+
+            var directory = new Directory
+            {
+                Name = "Test directory",
+                BriefDescription = "Brife description",
+                Category = new DirectoryCategory
+                {
+                    Name = "Medical directories"
+                },
+                XmlStructere =
+                    "<content > <customer><name>Wile E Coyote</name><address>The Desert</address></customer> " +
+                    "<orderItem><product>Rocket Powered Rollerskates</product>" +
+                    "<quantity>1</quantity><supplier>Acme Inc</supplier></orderItem></content>"
+            };
         }
     }
 }
---
+++ sources/Noris.Services/Utils/DynamicClassBuilder.cs
@@ -0,0 +1,70 @@
+﻿using System.Dynamic;
+using System.Xml.Linq;
+
+namespace Noris.Services.Utils
+{
+    /// <summary>
+    /// Building dynamic part of directory record structure
+    /// </summary>
+    public class DynamicClassBuilder : DynamicObject
+    {
+        XElement element;
+
+        public DynamicClassBuilder(string filename)
+        {
+            element = XElement.Load(filename);
+        }
+
+        private DynamicClassBuilder(XElement el)
+        {
+            element = el;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (element == null)
+            {
+                result = null;
+                return false;
+            }
+
+            XElement sub = element.Element(binder.Name);
+
+            if (sub == null)
+            {
+                result = null;
+                return false;
+            }
+            else
+            {
+                result = new DynamicClassBuilder(sub);
+                return true;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (element != null)
+            {
+                return element.Value;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public string this[string attr]
+        {
+            get
+            {
+                if (element == null)
+                {
+                    return string.Empty;
+                }
+
+                return element.Attribute(attr).Value;
+            }
+        }
+    }
+}
