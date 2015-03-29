0370838de815b5a0f8afe7fcc365bafe1ae7c9c8
zef <zef@kuzdrav.ru>
2015-03-29 14:12:46 +0700

    #4 Small fix

--- sources/Noris.WindowsForms/Form1.cs
+++ sources/Noris.WindowsForms/Form1.cs
@@ -19,26 +19,30 @@ namespace Noris.WindowsForms
     {
         public Form1()
         {
+
             InitializeComponent();
+
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-             string _xml = 
-                "<root>" +
-                    "<column name=\"FirstName\"></column>" +
-                    "<column name=\"LastName\"></column>" +
-                    "<column name=\"Blog\"></column>" +
-                "</root>";
+            string _xml =
+               "<root>" +
+                   "<column name=\"FirstName\"></column>" +
+                   "<column name=\"LastName\"></column>" +
+                   "<column name=\"Blog\"></column>" +
+               "</root>";
 
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(_xml);
-             
+
             //prepere any data for binding to DataGridView
             var list = new List<dynamic>();
-            
+
             for (int i = 0; i <= 10; i++)
             {
-                dynamic dyn = DynamicObjectBuilder.CreateOurNewObject(xmlDoc, typeof (RecordDtoExample));
+                dynamic dyn =  DynamicObjectBuilder.CreateOurNewObject(xmlDoc, typeof (RecordDtoExample));
+
+
                 dyn.FirstName = "Eugeni" + i.ToString();
 
                 dyn.LastName = "Zapalatin" + i.ToString();
@@ -50,13 +54,17 @@ namespace Noris.WindowsForms
                 dyn.Name = "Data" + i.ToString();
 
                 list.Add(dyn);
+
+
             }
 
             var listEnumer = (IEnumerable<dynamic>)list;
 
-            DataTable table = listEnumer.ToDataTable();
+            //DataTable table = listEnumer.ToDataTable();
+
             
-            dataGridView1.DataSource = table;
+            dataGridView1.DataSource = list;
+
 
         }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
